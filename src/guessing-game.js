1c0fbbbb7f66a3685510fab61e1f450f37ec9abc
Anniyak <anniyak@gmail.com>
2021-08-17 17:56:32 +0300

    init: start guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,21 @@
 class GuessingGame {
-    constructor() {}
+  constructor() { }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    //this method accepts min and max value of range of number to guess
+  }
 
-    }
+  guess() {
+    //this method returns solution candidate (you make an assumption based on range and previous assumptions)
+  }
 
-    guess() {
+  lower() {
+    //this method is called if prev call of `guess()` returned number which is greater than answer
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    //this method is called if prev call of `guess()` returned number which is lower than answer
+  }
 }
 
 module.exports = GuessingGame;
